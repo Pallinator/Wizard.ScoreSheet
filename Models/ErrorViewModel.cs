2073d78171c1ab9c6f7e4ca3e11f3257ac2e8bf3
pbricher20@gmail.com <pbricher20@gmail.com>
2022-01-08 13:06:50 -0700

    initial commit with all the files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wizard.ScoreSheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/PlayerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Wizard.ScoreSheet.Models
+{
+    public class PlayerModel
+    {
+        public string PlayerName { get; set; }
+        public int CurrentScore { get; set; }
+        public bool PlayerTurn { get; set; }
+        public int CurrentBid { get; set; }
+        public bool Dealer { get; set; }
+
+    }
+}
